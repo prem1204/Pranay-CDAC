899b74d74d8fc9a7021469e7e43c52b5482bc175
PRANAY PRALHAD GAVHALE <31377776+prem1204@users.noreply.github.com>
2020-05-02 10:05:16 +0530

    List and Dictionary pyton example

---
+++ listDict.py
@@ -0,0 +1,11 @@
+# List
+lsData=[10,20,30,40,50,60]
+print("List \n",lsData)
+
+# Dict
+dictData={1:"Jan",2:"Feb",3:"Mar",4:"Apr",5:'May',6:'Jun',
+7:'Jul',8:'Aug',9:'Sept',10:'Oct',11:'Nov',12:'Dec'}
+print("\nMonths")
+for k,v in dictData.items():
+    print(k,v)
+
